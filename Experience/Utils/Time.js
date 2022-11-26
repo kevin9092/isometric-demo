71f7e9fb988dd5929ba8a5491321eb075c69068a
Kevin Wilson <kevin9092@hotmail.co.za>
2022-11-26 18:40:37 +0000

    Added Stats.js and embedded into Dat-Gui

--- Experience/Utils/Time.js
+++ Experience/Utils/Time.js
@@ -1,4 +1,7 @@
 import { EventEmitter } from "events";
+import Stats from "stats.js";
+
+import Experience from "../Experience";
 
 export default class Time extends EventEmitter {
   constructor() {
@@ -7,15 +10,40 @@ export default class Time extends EventEmitter {
     this.current = this.start;
     this.elapsed = 0;
     this.delta = 16;
+    this.experience = new Experience();
+
+    // Stats
+    this.stats = new Stats();
+    this.stats.showPanel(1);
+    this.stats.domElement.height = "48px";
+    [].forEach.call(
+      this.stats.domElement.children,
+      (child) => (child.style.display = "")
+    );
+    // GUI Controls
+    if (this.experience.gui !== undefined) {
+      const perfFolder = this.experience.gui.addFolder("Performance Stats");
+      const perfLi = document.createElement("li");
+      this.stats.domElement.style.position = "static";
+      perfLi.appendChild(this.stats.domElement);
+      perfLi.classList.add("gui-stats");
+      perfFolder.__ul.appendChild(perfLi);
+    }
+    // document.body.appendChild(this.stats.dom);
 
     this.update();
   }
 
   update() {
     const currentTime = Date.now();
+
+    this.stats.begin();
+
     this.delta = currentTime - this.current;
     this.current = currentTime;
     this.elapsed = this.current - this.start;
+
+    this.stats.end();
 
     this.emit("update");
     window.requestAnimationFrame(() => this.update());

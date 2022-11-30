9e1d1ed192f4bfa075c99242cdabbf508eeadced
Kevin Wilson <kevin9092@hotmail.co.za>
2022-11-30 08:19:07 +0000

    Added Vercel Analytics

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import "./style.css";
 import Experience from "./Experience/Experience";
+import { inject } from "@vercel/analytics";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
+
+inject();
